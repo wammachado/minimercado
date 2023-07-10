e8cac5fdadddca7d12040ca76db28ec2e704b133
Wender Afonso <wammachado@gmail.com>
2023-07-10 01:05:34 -0300

    Update HomeController.php
    Removido funções nao utilizadas

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -27,55 +27,5 @@ class HomeController extends ProductModel
         require_once 'View/inc/footer.php';
     }
 
-    public function add()
-    {
-
-        
-        require_once 'View/Home/add.php';
-    }
-
-    public function save()
-    {
-        $name = $_POST['name'];
-        $price = $_POST['price'];
-        $description = $_POST['description'];
-        $result = $this->addProduct($name, $price, $description);
-        if ($result) {
-            header('Location: index.php');
-        } else {
-            echo "Error";
-        }
-    }
-
-    public function edit()
-    {
-        $id = $_GET['id'];
-        $product = $this->getProductById($id);
-        require_once 'View/Home/edit.php';
-    }
-
-    public function update()
-    {
-        $id = $_POST['id'];
-        $name = $_POST['name'];
-        $price = $_POST['price'];
-        $description = $_POST['description'];
-        $result = $this->updateProduct($id, $name, $price, $description);
-        if ($result) {
-            header('Location: index.php');
-        } else {
-            echo "Error";
-        }
-    }
-
-    public function delete()
-    {
-        $id = $_GET['id'];
-        $result = $this->deleteProduct($id);
-        if ($result) {
-            header('Location: index.php');
-        } else {
-            echo "Error";
-        }
-    }
+    
 }
